e9a6dddea28c2b3c1812f53a181ada5ef8dd830a
fiinfra-chetanhendre <chetan.hendre88@gmail.com>
2021-12-27 12:27:17 +0530

    added comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//this is main classs from where I will start project execution
 public class DemoApplication {
-
+		
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
